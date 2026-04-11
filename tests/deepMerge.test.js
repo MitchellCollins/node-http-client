4adc95bc25c45cc54a828ca34e6859402df300cf
Mitchell Collins <mitchell.collins@datapod.com.au>
2026-04-11 14:55:33 +1000

    27-fix-invalid-deep-merge

--- tests/deepMerge.test.js
+++ tests/deepMerge.test.js
@@ -11,8 +11,15 @@ describe("config merger", () => {
         const config1 = { port: 3000, headers: { "content-type": "application/json" }};
         const config2 = { timeout: 10000, headers: { "content-type": "text/plain", "content-length": Buffer.byteLength("Hello") }};
 
-        const expected = { port: 3000, timeout: 10000, headers: { "content-type": "text/plain", "content-length": Buffer.byteLength("Hello") }};
+        const expected1 = { port: 3000, timeout: 10000, headers: { "content-type": "text/plain", "content-length": Buffer.byteLength("Hello") }};
         
-        assert.deepStrictEqual(deepMerge(config1, config2), expected);
+        assert.deepStrictEqual(deepMerge(config1, config2), expected1);
+
+        const config3 = { headers: { accept: "*/*" } };
+        const config4 = { headers: { "content-type": "text/plain" }};
+
+        const expected2 = { headers: { ...config3.headers, ...config4.headers } };
+
+        assert.deepStrictEqual(deepMerge(config3, config4), expected2);
     });
 });

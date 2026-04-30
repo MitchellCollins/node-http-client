3ac7421ac382c9f2834462d0d0cf98dbd9ab8beb
Mitchell Collins <mitchell.collins@datapod.com.au>
2026-04-30 16:50:08 +1000

    46-fix-implement-input-validation-and-defaults

--- tests/defaults.test.js
+++ tests/defaults.test.js
@@ -5,9 +5,13 @@ const defaults = require("../lib/defaults");
 describe("default values", () => {
   it("should have defined default values", () => {
     assert.strictEqual(typeof defaults.baseURL, "string");
+    assert.strictEqual(typeof defaults.path, "string");
+    assert.strictEqual(typeof defaults.method, "object");
     assert.strictEqual(typeof defaults.config, "function");
     assert.strictEqual(typeof defaults.config(), "object");
     assert.strictEqual(typeof defaults.res, "function");
     assert.strictEqual(typeof defaults.res(), "object");
+    assert.strictEqual(typeof defaults.req, "function");
+    assert.strictEqual(typeof defaults.req(), "object");
   });
 });
